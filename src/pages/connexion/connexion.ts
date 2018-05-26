a20c40372bf8f04f6fbd8fc33b209d91bd3daa79
Angilye <ange.jacques@outlook.fr>
2018-05-26 12:06:03 +0200

    mise en place connexion API avec cloud9 et commencement de la gestion connexion

---
+++ src/pages/connexion/connexion.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { AcceuilPage } from '../acceuil/acceuil';
+
+import { BddService } from '../../services/bddapi.services';
+import { BddApiSignin } from '../../models/bddapi-signin.model';
+
+@Component({
+  selector: 'page-connexion',
+  templateUrl: 'connexion.html'
+})
+export class ConnexionPage {
+
+  login: string;
+  password: string;
+  error: string;
+
+  news: BddApiSignin = new BddApiSignin();
+
+  constructor(public navCtrl: NavController, private bddService: BddService) {
+    
+  }
+
+  Signin() {
+    console.log(this.login)
+    console.log(this.password)
+    this.bddService.getSignin(this.login, this.password)
+      .then(newsFetched => {
+        this.news = newsFetched;
+        console.log(this.news);
+        if (this.news.Success) {
+          this.goToAcceuil();
+        } else {
+          this.error = "Your login or password is false.";
+          alert('mal remplie');
+        }
+      });
+    
+  }
+
+  //goToAcceuil(params){
+  goToAcceuil() {
+    //if (!params) params = {};
+    this.navCtrl.push(AcceuilPage);
+  }
+}
---
+++ src/pages/m-odifierrapport/m-odifierrapport.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-m-odifierrapport',
+  templateUrl: 'm-odifierrapport.html'
+})
+export class MODIFIERRAPPORTPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
