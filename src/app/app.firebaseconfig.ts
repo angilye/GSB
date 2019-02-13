88cc4a6053f8291e19d433d6635faf2f10ff8b83
Angilye <ange.jacques@outlook.fr>
2019-02-13 12:07:04 +0100

    Suppression cle API Firebase

--- src/app/app.firebaseconfig.ts
+++ src/app/app.firebaseconfig.ts
@@ -1,5 +1,5 @@
  export var config = {
-    apiKey: "AIzaSyDVxZWTraK235eLgM4InutYpTzJ7SznLqA",
+    apiKey: "*******",
     authDomain: "g-gsb-c5f64.firebaseapp.com",
     databaseURL: "https://g-gsb-c5f64.firebaseio.com",
     projectId: "g-gsb-c5f64",
